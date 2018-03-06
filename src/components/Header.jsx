07612e8e463d92a52fbf821a369dc20d42e750a6
igorjouk <igrokk70@gmail.com>
2018-03-07 02:28:10 +0400

    create header and add it to component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <h1 className="App-header">TODO list</h1>
+    );
+  }
+}
